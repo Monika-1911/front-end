e7c0fdeeb9faa56ef018eec50001ecbed784bda9
Monika-1911 <mjp26997@gmail.com>
2025-09-05 18:07:42 +0530

    frontend

--- Task/Bootstrap/nav.js
+++ Task/Bootstrap/nav.js
@@ -50,5 +50,26 @@
           '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>",
         ]);
       });
+
+    var swiper = new Swiper('.swiper', {
+      slidesPerView: 3,
+      direction: getDirection(),
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        resize: function () {
+          swiper.changeDirection(getDirection());
+        },
+      },
+    });
+
+    function getDirection() {
+      var windowWidth = window.innerWidth;
+      var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+   return direction;
+    }
     </script>
     
