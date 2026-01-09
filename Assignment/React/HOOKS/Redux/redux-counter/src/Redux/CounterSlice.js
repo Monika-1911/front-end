7cd8f983859ccb76529d61fd66852f47325b79ea
Monika-1911 <mjp26997@gmail.com>
2026-01-09 11:13:53 +0530

    assignment

---
+++ Assignment/React/HOOKS/Redux/redux-counter/src/Redux/CounterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+// actions export કરો
+export const { increment, decrement } = counterSlice.actions;
+
+// reducer export કરો
+export default counterSlice.reducer;
